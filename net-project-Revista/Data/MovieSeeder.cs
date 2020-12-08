d143e024e57f73b73a9cfa71d40bc05f71f8db28
Nick Charvat <nickcharvat@gmail.com>
2020-12-08 13:38:20 -0800

    Added Category context and seed data

---
+++ net-project-Revista/Data/MovieSeeder.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using net_project_Revista.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_project_Revista.Data
+{
+    public class MovieSeeder
+    {
+        public static async Task SeedAsync(MovieDbContext movieContext)
+        {
+            if(!await movieContext.MovieCategories.AnyAsync())
+            {
+                await movieContext.MovieCategories.AddRangeAsync(
+                    GetPreconfiguredMovieCategories());
+
+                await movieContext.SaveChangesAsync();
+            }
+        }
+
+        static IEnumerable<MovieCategory> GetPreconfiguredMovieCategories()
+        {
+            return new List<MovieCategory>()
+            {
+                new MovieCategory("Popular"),
+                new MovieCategory("Top Rated"),
+                new MovieCategory("Now Playing"),
+                new MovieCategory("Upcoming")
+            };
+        }
+    }
+}
