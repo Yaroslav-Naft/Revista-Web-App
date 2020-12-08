4b449cbd799ddef2f96e3cae61849c3a1d85775f
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2020-12-08 12:06:38 -0800

    Created movie details page

---
+++ net-project-Revista/Pages/moviedetails.cshtml.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace net_project_Revista.Pages
+{
+    public class moviedetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage();
+        }
+
+    }
+}
