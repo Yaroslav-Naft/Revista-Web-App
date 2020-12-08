c3cd78ae65c3e527919549f1c50b4448143dd3ea
Nick Charvat <nickcharvat@gmail.com>
2020-12-08 13:32:47 -0800

    Added Category and Genre models

---
+++ net-project-Revista/Models/MovieGenre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_project_Revista.Models
+{
+    public class MovieGenre : BaseEntity
+    {
+        public string Genre { get; set; }
+
+        // navigation properties
+        public virtual ICollection<Movie> GenreMovies { get; set; }
+
+        public MovieGenre(string genre)
+        {
+            Genre = genre;
+        }
+    }
+}
