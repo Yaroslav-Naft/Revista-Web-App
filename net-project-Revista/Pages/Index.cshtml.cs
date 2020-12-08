3e563dfbe8fb410775622c8466d9c313a9b2bd17
Nick Charvat <nickcharvat@gmail.com>
2020-12-08 15:17:14 -0800

    #32 created movie view model

--- net-project-Revista/Pages/Index.cshtml.cs
+++ net-project-Revista/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using net_project_Revista.Data;
 using net_project_Revista.Models;
 using System;
 using System.Collections.Generic;
@@ -11,31 +12,17 @@ namespace net_project_Revista.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
+        private readonly MovieDbContext _db;
        
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(MovieDbContext db)
         {
-            _logger = logger;
+            _db = db;
         }
 
-        public int Reps { get; set; } = 5;
-
-        public List<Movie> Movies { get; set; } = new List<Movie>();
+        public ICollection<Movie> Movies { get; private set; }
 
         public void OnGet()
         {
-            //test with hard code movie
-            Movies = new List<Movie>()
-            {
-                    new Movie("Jiu jitsu","Every six years, an ancient order of jiu-jitsu fighters joins forces to battle a vicious race of ", DateTime.Now ,"Action, Fantasy, Science Fiction", 142, "/images/photo-1.jpg","Popular"),
-                    new Movie("Uside Down Magic","Nory and her best friend Reina enter the Sage Academy for Magical Studies, where Nory’s unconventional powers land her in a class ", DateTime.Now ,"Family, Fantasy, TV Movie", 134, "/images/photo-2.pg","Now Playing"),
-                    new Movie("Joker","During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming ", DateTime.Now ,"Crime, Thriller, Drama", 220, "/images/photo-3.jpg","Popular"),
-                    new Movie("Jiu jitsu","Every six years, an ancient order of jiu-jitsu fighters joins forces to battle a vicious race of ", DateTime.Now ,"Action, Fantasy, Science Fiction", 142, "/images/photo-4.jpg","Popular"),
-                    new Movie("Jiu jitsu","Every six years, an ancient order of jiu-jitsu fighters joins forces to battle a vicious race of ", DateTime.Now ,"Action, Fantasy, Science Fiction", 142, "/images/photo-5.jpg","Popular"),
-                    new Movie("Jiu jitsu","Every six years, an ancient order of jiu-jitsu fighters joins forces to battle a vicious race of ", DateTime.Now ,"Action, Fantasy, Science Fiction", 142, "/images/photo-6.jpg","Popular"),
-
-            };
         }
     }
 }
