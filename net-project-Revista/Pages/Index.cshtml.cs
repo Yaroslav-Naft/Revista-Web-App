7c6f2145c0d455a56fe906b677e0912f54bd3175
Nick Charvat <nickcharvat@gmail.com>
2020-12-08 16:00:52 -0800

    #32 updated index.cshtml.cs with movie service and movie index

--- net-project-Revista/Pages/Index.cshtml.cs
+++ net-project-Revista/Pages/Index.cshtml.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using net_project_Revista.Data;
+using net_project_Revista.Interfaces;
 using net_project_Revista.Models;
+using net_project_Revista.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,17 +14,22 @@ namespace net_project_Revista.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IMovieVMService _movieVMService;
         private readonly MovieDbContext _db;
        
-        public IndexModel(MovieDbContext db)
+        public IndexModel(IMovieVMService movieVMService, MovieDbContext db)
         {
+            _movieVMService = movieVMService;
             _db = db;
         }
 
+        public MovieIndexVM MovieIndex = new MovieIndexVM();
+
         public ICollection<Movie> Movies { get; private set; }
 
-        public void OnGet()
+        public void OnGet(MovieIndexVM movieIndex)
         {
+            MovieIndex = _movieVMService.GetMoviesVM(movieIndex.CategoriesFilterApplied);
         }
     }
 }
