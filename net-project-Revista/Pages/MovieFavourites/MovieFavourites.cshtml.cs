32b3ccb39731cd28634739ae6aebab0a0aebc623
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2020-12-10 10:54:19 -0800

    created initial favourites page #73

---
+++ net-project-Revista/Pages/MovieFavourites/MovieFavourites.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace net_project_Revista.Pages.MovieFavourites
+{
+    public class MovieFavouritesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
