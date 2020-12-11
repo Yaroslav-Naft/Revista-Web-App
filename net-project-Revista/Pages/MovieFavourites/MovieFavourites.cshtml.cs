802b25e62afafecebbd469581cd45a942b994550
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2020-12-10 22:15:40 -0800

    Fixed Issue with favourite movies #73

--- net-project-Revista/Pages/MovieFavourites/MovieFavourites.cshtml.cs
+++ net-project-Revista/Pages/MovieFavourites/MovieFavourites.cshtml.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using net_project_Revista.Data;
 using net_project_Revista.Models;
 using net_project_Revista.ViewModels;
@@ -13,17 +15,27 @@ namespace net_project_Revista.Pages.MovieFavourites
     public class moviefavouritesModel : PageModel
     {
         private readonly MovieDbContext _db;
-        public Movie Movie { get; private set; }
+        public Favourite Favourite { get; set; }
         public string PosterPath { get; private set; }
         public moviefavouritesModel(MovieDbContext db)
         {
             _db = db;
         }
+        public ICollection<FavouriteMovie> FavouriteMoviesList { get; private set; }
 
         public void OnGet()
         {
-            
+         //Get Favourite movies from database
+         Favourite = _db.Favourites
+        .Include(c => c.FavouriteMovies)
+        .ThenInclude(c => c.Movie)
+        .Where(c => c.Id == (int)HttpContext.Session.GetInt32("favouriteId"))
+        .FirstOrDefault();
 
+
+
+            //Set the movies variable to a collection
+            FavouriteMoviesList = Favourite.FavouriteMovies;
 
 
             //how do you directly get data from database not how you get from a post
