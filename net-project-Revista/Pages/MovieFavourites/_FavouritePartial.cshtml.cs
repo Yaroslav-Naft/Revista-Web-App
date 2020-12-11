514163e2ee80e0ce41e4a0cb91e588639b0f062f
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2020-12-11 10:22:51 -0800

    rendering movie for issue #73

---
+++ net-project-Revista/Pages/MovieFavourites/_FavouritePartial.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using net_project_Revista.Data;
+using net_project_Revista.Models;
+
+namespace net_project_Revista.Pages.MovieFavourites
+{
+    public class _FavouritePartialModel : PageModel
+    {
+        private readonly net_project_Revista.Data.MovieDbContext _context;
+
+        public _FavouritePartialModel(net_project_Revista.Data.MovieDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<FavouriteMovie> FavouriteMovie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            FavouriteMovie = await _context.FavouriteMovies
+                .Include(f => f.Favourite)
+                .Include(f => f.Movie).ToListAsync();
+        }
+    }
+}
